588d5b554ab88736bcf761d000eccf799328d631
Garima-27 <garima1051.cse18@chitkara.edu.in>
2020-11-07 19:42:31 +0530

    initial commit

---
+++ FullStack/src/com/wipro/flight/bean/Sales.java
@@ -0,0 +1,51 @@
+package com.wipro.flight.bean;
+
+import java.util.*;
+
+public class Sales {
+	String salesId;
+	String productID;
+    Date salesDate;
+	int quantitySold;
+	double salesPricePerUnit;
+
+	public String getSalesId() {
+		return salesId;
+	}
+
+	public void setSalesId(String salesId) {
+		this.salesId = salesId;
+	}
+
+	public String getProductID() {
+		return productID;
+	}
+
+	public void setProductID(String productID) {
+		this.productID = productID;
+	}
+
+	public int getQuantitySold() {
+		return quantitySold;
+	}
+
+	public void setQuantitySold(int quantitySold) {
+		this.quantitySold = quantitySold;
+	}
+
+	public double getSalesPricePerUnit() {
+		return salesPricePerUnit;
+	}
+
+	public void setSalesPricePerUnit(double salesPricePerUnit) {
+		this.salesPricePerUnit = salesPricePerUnit;
+	}
+
+	public Date getSalesDate() {
+		return salesDate;
+	}
+
+	public void setSalesDate(Date salesDate) {
+		this.salesDate = salesDate;
+	}
+}
